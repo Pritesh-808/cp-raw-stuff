2e585aef6299c5a70972ad7830a8e8cac9bb945f
epsilord <66950210+epsilord@users.noreply.github.com>
2020-10-17 23:16:19 +0530

    Create c++__ template.cpp

---
+++ c++__ template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define dd double
+#define pb push_back
+#define For(i,n) for(int i=0;i<n;i++)
+#define FOR(i,n) for(ll  i=0;i<n;i++)
+void accept(int arr[], ll size){for(ll h=0;h<size;h++){cin>>arr[h];}}
+void accept_vector(vector<ll>&arr,ll n){for(ll i =0;i<n;i++){cin>>arr[i];}}
+
+int main() {
+   ll T;
+   cin>>T;
+   FOR(test_case,T){
+       
+       
+       
+         
+          
+             
+          
+
+  
+
+	
+}
+}
