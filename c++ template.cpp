cf9121b3ba72a617da603234c951319f7f174dee
epsilord <66950210+epsilord@users.noreply.github.com>
2020-10-19 17:21:39 +0530

    Update c++ template.cpp

--- c++ template.cpp
+++ c++ template.cpp
@@ -7,21 +7,23 @@ using namespace std;
 #define FOR(i,n) for(ll  i=0;i<n;i++)
 void accept(int arr[], ll size){for(ll h=0;h<size;h++){cin>>arr[h];}}
 void accept_vector(vector<ll>&arr,ll n){for(ll i =0;i<n;i++){cin>>arr[i];}}
-
+void showlist(list<ll>ans){for(auto it=ans.begin();it!=ans.end();it++) cout<<*it<<" ";}
+bool check(vector<ll>&arr,ll n){
+    ll ref=arr[0];
+    FOR(i,n){
+        if(arr[i]!=ref)
+        return false;
+    }
+    return true;
+}
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);    
+	
    ll T;
    cin>>T;
    FOR(test_case,T){
-       
-       
-       
-         
-          
-             
-          
-
-  
-
-	
+    
 }
 }
