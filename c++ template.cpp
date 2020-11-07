7e168bf696f861067b771d431695f5d7789afb34
epsilord <66950210+epsilord@users.noreply.github.com>
2020-11-07 22:58:17 +0530

    Update c++ template.cpp

--- c++ template.cpp
+++ c++ template.cpp
@@ -5,18 +5,26 @@ using namespace std;
 #define pb push_back
 #define For(i,n) for(int i=0;i<n;i++)
 #define FOR(i,n) for(ll  i=0;i<n;i++)
+#define all(X) X.begin(),X.end()
+#define vt vector<long long>
 void accept(int arr[], ll size){for(ll h=0;h<size;h++){cin>>arr[h];}}
 void accept_vector(vector<ll>&arr,ll n){for(ll i =0;i<n;i++){cin>>arr[i];}}
 void showlist(list<ll>ans){for(auto it=ans.begin();it!=ans.end();it++) cout<<*it<<" ";}
+void showstack(stack<ll>s){if(s.empty())return;int x=s.top();s.pop();showstack(s);cout <<x<<" ";s.push(x);}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);    
+    cout.tie(0);
+    	
+    ll T;
+    cin>>T;
+    FOR(test_case,T)
+    {
+	    
+	    
+	    
+    }	
 	
-   ll T;
-   cin>>T;
-   FOR(test_case,T){
-    
-}
+	   
 }
