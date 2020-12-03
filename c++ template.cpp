2f8e4c88ba785d015d7d2a08ce763719738d08fe
epsilord <66950210+epsilord@users.noreply.github.com>
2020-12-03 18:52:28 +0530

    Update c++ template.cpp

--- c++ template.cpp
+++ c++ template.cpp
@@ -7,24 +7,32 @@ using namespace std;
 #define FOR(i,n) for(ll  i=0;i<n;i++)
 #define all(X) X.begin(),X.end()
 #define vt vector<long long>
+#define printll(x) printf("%lld",x)
 void accept(int arr[], ll size){for(ll h=0;h<size;h++){cin>>arr[h];}}
 void accept_vector(vector<ll>&arr,ll n){for(ll i =0;i<n;i++){cin>>arr[i];}}
+void accept_vector2d(vector<vector<ll>>&a,ll x,ll y){for(ll i=0;i<x;i++)for(ll j=0;j<y;j++)cin>>a[i][j];}
 void showlist(list<ll>ans){for(auto it=ans.begin();it!=ans.end();it++) cout<<*it<<" ";}
 void showstack(stack<ll>s){if(s.empty())return;int x=s.top();s.pop();showstack(s);cout <<x<<" ";s.push(x);}
-
+ll euclid_gcd(ll a,ll b){ll divident=(a>=b)?a:b;ll divisor =(a>=b)?b:a;while(divisor!=0){ll rem=divident%divisor;divident=divisor;divisor=rem;}return divident;}
+/*clock_t tStart = clock();
+printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC); */
+//printf("Case #%lld: %lld",test_case+1,ans);
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    	
+
     ll T;
     cin>>T;
     FOR(test_case,T)
     {
-	    
-	    
-	    
-    }	
-	
-	   
+
+
+
+
+
+
+    }
+
+
 }
